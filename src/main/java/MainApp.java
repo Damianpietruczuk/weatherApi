fb83a73df549569947bf3b63d8498f449cbc2104
Damian <d.pietrucuzk@gmail.com>
2019-10-18 16:13:29 +0200

    add json use

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,11 +1,19 @@
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class MainApp {
+
     public static void main(String[] args) {
+
         try {
-            System.out.println(new HttpService().connect("http://"+ApiConfig.APIURL+"&appid="+ApiConfig.ID));
+
+            JSONObject obj = new JSONObject(new HttpService().connect("http://"+ApiConfig.APIURL+"&appid="+ApiConfig.ID));
+            System.out.println(obj);
+            System.out.println(obj.get("visibility"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
